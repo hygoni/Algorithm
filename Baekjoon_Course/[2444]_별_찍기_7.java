86af9c2733bfe383939f65f9faf92a0952ea7c0f
hygoni <9hrack@naver.com>
2019-06-28 15:20:58 +0000

    별 찍기 7,8

---
+++ Baekjoon_Course/[2444]_별_찍기_7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int height = input.nextInt();
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < height-i-1; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < 2*i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i = height-1; i > 0; i--) {
+			for(int j = 0; j < height-i ; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 2*i-1; j > 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
