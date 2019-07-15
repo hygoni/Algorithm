78dc357a075bedc2a3556cf6b98b38dfb380ab66
hygoni <9hrack@naver.com>
2019-07-15 05:22:14 +0000

    2xn 타일링 2

---
+++ Baekjoon_Course/3.Dynamic_Programming/[11727]_2xn_타일링_2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
+                int n = input.nextInt();
+                int[] m = new int[n+1];
+                m[0] = 1;
+                m[1] = 1;
+                for(int i = 2; i <= n; i++) {
+                        m[i] = ( m[i-1] + 2 * m[i-2] ) % 10007;
+                }
+                System.out.println(m[n]);
+        }
+
+
+}
+
