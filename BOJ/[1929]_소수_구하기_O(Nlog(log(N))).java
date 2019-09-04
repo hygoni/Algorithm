3a1dc0c6de315c264ead71f66420d49641be3b2a
hygoni <9hrack@naver.com>
2019-09-04 18:55:52 +0000

    edited some

--- BOJ/[1929]_소수_구하기_O(Nlog(log(N))).java
+++ BOJ/[1929]_소수_구하기_O(Nlog(log(N))).java
@@ -7,16 +7,6 @@ import java.util.Scanner;
 
 public class Main {
 	
-	public static boolean isPrime(int n) {
-		if(n == 1) return false;
-		
-		for(int i = 2; i*i <= n; i++) {
-			if(n % i == 0)
-				return false;
-		}
-		return true;
-	}
-	
 	public static void main(String[] args) throws IOException {
 		Scanner input = new Scanner(System.in);
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
