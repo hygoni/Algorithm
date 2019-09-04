207c383a351035a4cb75eb71a934cb1f64298688
hygoni <9hrack@naver.com>
2019-09-04 18:49:40 +0000

    소수구하기 개선

---
+++ BOJ/[1929]_소수_구하기_O(Nlog(log(N))).java
@@ -0,0 +1,51 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+//Time Complexity : O(Nlog(log(N))
+
+public class Main {
+	
+	public static boolean isPrime(int n) {
+		if(n == 1) return false;
+		
+		for(int i = 2; i*i <= n; i++) {
+			if(n % i == 0)
+				return false;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Scanner input = new Scanner(System.in);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int m = input.nextInt();
+		int n = input.nextInt();
+		
+		boolean isPrime[] = new boolean[n+1];
+		isPrime[1] = true;
+		
+		for(int i = 2; i <= n; i++) {
+			if(isPrime[i]) {
+				continue;
+			}
+			for(int j = i+i; j <= n; j += i) {
+				isPrime[j] = true;
+			}
+		}
+		
+		for(int i = m; i <= n; i++) {
+			if(isPrime[i] == false) {
+				bw.write(i + "\n");
+			}
+		}
+		
+		bw.flush();
+		bw.close();
+		input.close();
+	}
+	
+	
+}
+
