aacd120e0f2e841d22ca182f76a9da13b71e4b1e
hygoni <9hrack@naver.com>
2019-10-01 22:07:15 +0900

    입국심사

---
+++ Kakao/코딩테스트_연습/이분탐색/입국심사.java
@@ -0,0 +1,37 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+
+class Solution {
+	public static void main(String[] args) {
+		int times[] = {7, 10};
+		int n = 6;
+		Solution solver = new Solution();
+		long ans = solver.solution(n, times);
+		System.out.println(ans);
+	}
+	
+	public long solution(int n, int[] times) {
+		Arrays.sort(times);
+		BigInteger start = BigInteger.ZERO;
+		BigInteger end = new BigInteger(times[times.length-1] + "").multiply(new BigInteger(n + ""));
+		BigInteger min = new BigInteger(Long.MAX_VALUE + "");
+		while(start.compareTo(end) <= 0) {
+			BigInteger mid = (start.add(end)).divide(new BigInteger("2"));
+			BigInteger people = BigInteger.ZERO;
+			for(int time : times) {
+				people = people.add(mid.divide(new BigInteger(time + "")));
+			}
+			
+			if(people.compareTo(new BigInteger(n + "")) >= 0) {
+				if(min.compareTo(mid) > 0) {
+					min = mid;
+				}
+				end = mid.subtract(BigInteger.ONE);
+			} else {
+				start = mid.add(BigInteger.ONE);
+			}
+		}
+		long answer = min.longValue();
+		return answer;
+	}
+}
