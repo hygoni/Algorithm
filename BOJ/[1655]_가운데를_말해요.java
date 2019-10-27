16b1379c6ea77ee437a6c83b6579f7982a61dac3
hygoni <9hrack@naver.com>
2019-10-27 20:40:46 +0900

    가운데를 말해요 가독성 Up

--- BOJ/[1655]_가운데를_말해요.java
+++ BOJ/[1655]_가운데를_말해요.java
@@ -8,29 +8,25 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(1, Collections.reverseOrder());
-		PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+		PriorityQueue<Integer> max = new PriorityQueue<Integer>(1, Collections.reverseOrder());
+		PriorityQueue<Integer> min = new PriorityQueue<Integer>();
 		StringBuilder sb = new StringBuilder();
 		
 		for(int i = 0; i < n; i++) {
 			int num = Integer.parseInt(br.readLine());
-			if(maxHeap.size() <= minHeap.size()) {
-				maxHeap.add(num);
-			} else {
-				minHeap.add(num);
+			min.add(num);
+			max.add(min.poll());
+			
+			if(min.size() < max.size()) {
+				min.add(max.poll());
 			}
 			
-			if(maxHeap.size() > 0 && minHeap.size() > 0 && maxHeap.peek() > minHeap.peek()) {
-				minHeap.add(maxHeap.poll());
-				maxHeap.add(minHeap.poll());
-			}
-			
-			int size = maxHeap.size() + minHeap.size();
-			if(size % 2 == 0) {
-				sb.append(Math.min(maxHeap.peek(), minHeap.peek())).append("\n");
+			if(min.size() > max.size()) {
+				sb.append(min.peek());
 			} else {
-				sb.append(maxHeap.peek()).append("\n");
+				sb.append(Math.min(min.peek(), max.peek()));
 			}
+			sb.append("\n");
 		}
 		System.out.println(sb);
 		br.close();
