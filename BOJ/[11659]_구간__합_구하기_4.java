a840e9617a80b8c2d12cfd75329d93648f4420c2
Jade Yu <50040414+hygoni@users.noreply.github.com>
2020-01-10 03:17:06 +0900

    구간 합 구하기 4

---
+++ BOJ/[11659]_구간__합_구하기_4.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int arr[] = new int[N+1];
+		int sum[] = new int[N+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum[i] = sum[i-1] + arr[i];
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int i_ = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			System.out.println(sum[j] - sum[i_-1]);
+		}
+		
+		br.close();
+	}
+}
